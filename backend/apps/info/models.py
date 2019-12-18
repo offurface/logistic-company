42fb48244d99fcc1add4fe56b5123a7e4f298a16
Владимир Киселев <40614410+offurface@users.noreply.github.com>
2019-12-18 15:01:52 +0300

    Update models.py

--- backend/apps/info/models.py
+++ backend/apps/info/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=100, verbose_name="Сокращенное наименование")
     full_name = models.CharField(max_length=255, verbose_name="Полное наименование")
-
+    
+    class Meta:
+        verbose_name = "Организация"
+        verbose_name_plural = "Организации"
     
 #  Товары
 class Goods(models.Model):
@@ -13,11 +16,19 @@ class Goods(models.Model):
     x = models.DecimalField(max_digits=100, decimal_places=2, verbose_name="Ширина (м.)")
     y = models.DecimalField(max_digits=100, decimal_places=2, verbose_name="Длинна (м.)")
     z = models.DecimalField(max_digits=100, decimal_places=2, verbose_name="Высота (м.)")
+    
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
 
 
 #  Транспорт
 class Transport(models.Model):
     mark = models.CharField(max_length=100, verbose_name="Наименование")
+    
+    class Meta:
+        verbose_name = "Транспорт"
+        verbose_name_plural = "Транспортные средства"
 
 #  Водители
 class Driver(models.Model):
@@ -44,24 +55,37 @@ class Driver(models.Model):
     surname = models.CharField(max_length=150, verbose_name="Фамилия")
     patronymic = models.CharField(max_length=150, verbose_name="Отчество")
     category = models.CharField(max_length=4, choices=DRIVERS_LICENSE , verbose_name="Категория")
-
+    
+    class Meta:
+        verbose_name = "Водитель"
+        verbose_name_plural = "Водители"
 #  Населенные пункты
 class Adres(models.Model):
     city = models.CharField(max_length=100, verbose_name="Город")
     street = models.CharField(max_length=100, verbose_name="Улица, № Дома")
+    class Meta:
+        verbose_name = "Населенные пункт"
+        verbose_name_plural = "Населенные пункты"
 
 #  Исполнители частные лица
 class ExecutorPerson(models.Model):
     name = models.CharField(max_length=150, verbose_name="Имя")
     surname = models.CharField(max_length=150, verbose_name="Фамилия")
     patronymic = models.CharField(max_length=150, verbose_name="Отчество")
-
+    
+    class Meta:
+        verbose_name = "Физическое лицо"
+        verbose_name_plural = "Физические лица"
 
 #  Исполнители юридические лица
 class ExecutorLegal(models.Model):
     name = models.CharField(max_length=100, verbose_name="Сокращенное наименование")
     full_name = models.CharField(max_length=255, verbose_name="Полное наименование")
-
+    
+    class Meta:
+        verbose_name = "Юридическое лицо"
+        verbose_name_plural = "Юридические лица"
+        
 #  Клиенты (физ. лица)
 class ClientPerson(models.Model):
     name = models.CharField(max_length=150, verbose_name="Имя")
@@ -70,16 +94,28 @@ class ClientPerson(models.Model):
     address = models.CharField(max_length=255, verbose_name="Адрес")
     inn = models.CharField(max_length=255, verbose_name="ИНН")
     phone = models.CharField(max_length=25, verbose_name="Телефон")
-
+    
+    class Meta:
+        verbose_name = "Физическое лицо"
+        verbose_name_plural = "Физические лица"
+        
+        
 #  Клиенты (юр лица) 
 class ClientLegal(models.Model):
     name = models.CharField(max_length=100, verbose_name="Сокращенное наименование")
     full_name = models.CharField(max_length=255, verbose_name="Полное наименование")    
     address = models.CharField(max_length=255, verbose_name="Адрес")
     inn = models.CharField(max_length=255, verbose_name="ИНН")
-    phone = models.CharField(max_length=25, verbose_name="Телефон")    
+    phone = models.CharField(max_length=25, verbose_name="Телефон")  
+    
+    class Meta:
+        verbose_name = "Юридическое лицо"
+        verbose_name_plural = "Юридические лица"
     
 #  Тарифы
 class Tariff(models.Model):
     name = models.CharField(max_length=50, verbose_name="Наименование")
     price = models.DecimalField(max_digits=100, decimal_places=2, verbose_name="Цена")
+    class Meta:
+        verbose_name = "Тариф"
+        verbose_name_plural = "Тарифы"
